8f33cd7c33169924a73a38cb47ba515ed3e75c6f
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-03-10 13:23:54 +0100

    Condition added to refresh tree 	modified:   tmux_current_tree.sh

--- tmux_current_tree.sh
+++ tmux_current_tree.sh
@@ -5,9 +5,13 @@
 # https://research.pasteur.fr/en/member/guillaume-bouvier/
 # 2020-03-10 10:34:11 (UTC+0100)
 
+DISPLAYDIR='None'
 while :; do
     PANEDIR=$(tmux display-message -p -F '#{pane_current_path}')
-    clear
-    exa -snew -T -L 1 --color='always' "$PANEDIR"
+    if [ $DISPLAYDIR != $PANEDIR ]; then
+        clear
+        exa -snew -T -L 2 --color='always' "$PANEDIR"
+    fi
+    DISPLAYDIR=$PANEDIR
     sleep 1
 done
