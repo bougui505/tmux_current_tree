07d47c4df6c5b9f2a981119c1fe3f0975cd69ac5
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-03-10 10:59:42 +0100

    Initial commit 	new file:   tmux_current_tree.sh

---
+++ tmux_current_tree.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-03-10 10:34:11 (UTC+0100)
+
+while :; do
+    PANEDIR=$(tmux display-message -p -F '#{pane_current_path}')
+    clear
+    exa -T --color='always' "$PANEDIR"
+    sleep 1
+done
