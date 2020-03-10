0466b1ffdf45158195ebc626696dc878f87c41ac
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-03-10 11:20:28 +0100

    new file:   tmux-sidebar.sh

---
+++ tmux-sidebar.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-03-10 11:17:08 (UTC+0100)
+
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+cd $DIRSCRIPT
+tmux -2 splitw -h ./tmux_current_tree.sh
+tmux last-pane
