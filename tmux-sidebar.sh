f30b6472d1354f05d2878222d647f8d579123a3b
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-03-10 11:44:26 +0100

    Kill the pane if already exists 	modified:   tmux-sidebar.sh

--- tmux-sidebar.sh
+++ tmux-sidebar.sh
@@ -5,6 +5,13 @@
 # https://research.pasteur.fr/en/member/guillaume-bouvier/
 # 2020-03-10 11:17:08 (UTC+0100)
 
+CURPID=$(pgrep -f tmux_current_tree.sh | sort | head -1)
+if [ ! -z $CURPID ]; then
+    PANEID=$(tmux list-panes -a -F "#{pane_id} #{pane_pid}" | grep $CURPID | awk '{print $1}')
+    tmux kill-pane -t $PANEID
+    exit
+fi
+
 DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
 cd $DIRSCRIPT
 tmux -2 splitw -l 40 -h ./tmux_current_tree.sh
